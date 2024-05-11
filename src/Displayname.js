90e934dd61c7d5f195ba52e8cf2c5c03f6472a62
Saneep dalal <171ankit.2020@gmail.com>
2024-05-11 13:10:14 +0530

    add

---
+++ src/Displayname.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+export default function DisplayName(){
+    const [submit,setSubmit]=useState(false);
+    const [firstname,setFirstname] = useState("");
+    const [lastname,setLastname] = useState("");
+    const [fullname,setFullname] = useState("");
+
+
+    const handleFirstname =(e) =>{
+        setFirstname(e.target.value)
+    }
+    const handleLastname =(e) =>{
+        setLastname(e.target.value)
+    }
+
+    const handleSubmit =(e) =>{
+        e.preventDefault();
+        console.log(e);
+        setFullname(`${firstname} ${lastname}`);
+        setSubmit(true)
+    }
+
+    return (
+        <>
+        <form >
+            <h1>Full Name Display</h1>
+            <label> First Name:</label>
+            <input type="text" onChange={handleFirstname} required></input><br></br>
+            <label> Last Name:</label>
+            <input type="text" onChange={handleLastname} required></input><br></br>
+            <button onClick={handleSubmit}> Submit</button>
+            {submit ? <div>Full Name : {fullname}</div>:""}
+        </form>
+        </>
+    )
+}
